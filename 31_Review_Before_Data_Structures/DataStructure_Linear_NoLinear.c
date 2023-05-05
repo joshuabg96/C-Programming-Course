f5b4a4939fbc8206efe1a04049edf458123e8685
joshuabg96 <josuebg96@hotmail.com>
2023-05-04 20:48:48 -0600

    Changes missing

---
+++ 31_Review_Before_Data_Structures/DataStructure_Linear_NoLinear.c
@@ -0,0 +1,6 @@
+/*
+    A linear dinamic structure has only one before and after element except for the frist and last
+
+    A no linear dinamic structure has a unique element before, except for the first and can have 2 after
+    
+*/
